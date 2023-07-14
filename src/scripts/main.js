609078b2d98952421e131580d34ba78723733290
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-07-14 12:49:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const feedback = document.querySelector('.feedback__slider');
+const feedback_offset = 300;
+
+function feedbackUpdateHeight(height) {
+  if (!height) return false;
+  feedback.style.height = `${height + feedback_offset}px`;
+}
+
+if (feedback) {
+  feedbackUpdateHeight(feedback.querySelector('.feedback__item').offsetHeight);
+
+  const feedback_slider = new Swiper(feedback, {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    autoHeight: true,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  feedback_slider.on('slideChange', () => {
+    setTimeout(() => {
+      feedbackUpdateHeight(feedback_slider.slides[feedback_slider.activeIndex].offsetHeight);
+    }, 300);
+  });
+}
